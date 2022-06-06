445f390193b2fe0fd7fa35d98d1ef5c74a0689be
shh <tenstars@foxmail.com>
2022-06-06 11:40:40 +0800

    资源打包后自动生成版本文件

--- Assets/Game/Scripts/Editor/Utility/IOUtility.cs
+++ Assets/Game/Scripts/Editor/Utility/IOUtility.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 
 namespace Game.Editor
 {
@@ -9,6 +10,21 @@ namespace Game.Editor
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
+            }
+        }
+
+        public static void SaveFileSafe(string path, string fileName, string text)
+        {
+            CreateDirectoryIfNotExists(path);
+
+            string filePath = Path.Combine(path, fileName);
+            using (FileStream stream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                UTF8Encoding utf8Encoding = new UTF8Encoding(false);
+                using (StreamWriter writer = new StreamWriter(stream, utf8Encoding))
+                {
+                    writer.Write(text);
+                }
             }
         }
     }
