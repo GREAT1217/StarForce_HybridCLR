19b3fe834e4e270d3568c05e5c8c1c8ec85e38be
shh <tenstars@foxmail.com>
2022-06-24 18:07:01 +0800

    更新huatuo

--- Assets/Game/Scripts/Hotfix/GameHotfixEntry.cs
+++ Assets/Game/Scripts/Hotfix/GameHotfixEntry.cs
@@ -1,4 +1,5 @@
-﻿using GameFramework;
+﻿using System;
+using GameFramework;
 using GameFramework.Fsm;
 using GameFramework.Procedure;
 using GameFramework.Resource;
@@ -15,7 +16,48 @@ namespace Game.Hotfix
             private set;
         }
 
-        public static void Start()
+        public static readonly string[] AOTDllNames = new string[]
+        {
+            "mscorlib.dll",
+            "System.dll",
+            "System.Core.dll", // 如果使用了Linq，需要这个
+            // "Newtonsoft.Json.dll",
+            // "protobuf-net.dll",
+            // "Google.Protobuf.dll",
+            // "MongoDB.Bson.dll",
+            // "DOTween.Modules.dll",
+            // "UniTask.dll",
+        };
+
+        private static int AOTFlag;
+        private static int AOTLoadFlag;
+
+        public static unsafe void Start()
+        {
+#if UNITY_EDITOR
+            StartHotfix();
+#else
+            // 为aot assembly加载原始metadata， 这个代码放aot或者热更新都行。
+            // 一旦加载后，如果AOT泛型函数对应native实现不存在，则自动替换为解释模式执行。
+
+            // 可以加载任意aot assembly的对应的dll。但要求dll必须与unity build过程中生成的裁剪后的dll一致，而不能直接使用原始dll。
+            // 我们在Huatuo_BuildProcessor_xxx里添加了处理代码，这些裁剪后的dll在打包时自动被复制到 {项目目录}/HuatuoData/AssembliesPostIl2CppStrip/{Target} 目录。
+
+            // 注意，补充元数据是给AOT dll补充元数据，而不是给热更新dll补充元数据。
+            // 热更新dll不缺元数据，不需要补充，如果调用LoadMetadataForAOTAssembly会返回错误。
+
+            AOTFlag = AOTDllNames.Length;
+            AOTLoadFlag = 0;
+            for (int i = 0; i < AOTFlag; i++)
+            {
+                string dllName = AOTDllNames[i];
+                string assetName = Utility.Text.Format("Assets/Game/Hotfix/{0}.bytes", dllName);
+                GameEntry.Resource.LoadAsset(assetName, new LoadAssetCallbacks(OnLoadAOTDllSuccess, OnLoadAssetFail));
+            }
+#endif
+        }
+
+        private static void StartHotfix()
         {
             // 删除原生对话框。
             GameEntry.BuiltinData.DestroyDialog();
@@ -37,9 +79,26 @@ namespace Game.Hotfix
             GameEntry.Resource.LoadAsset("Assets/Game/Game.prefab", new LoadAssetCallbacks(OnLoadAssetSuccess, OnLoadAssetFail));
         }
 
+        private static unsafe void OnLoadAOTDllSuccess(string assetName, object asset, float duration, object userdata)
+        {
+            TextAsset dll = (TextAsset)asset;
+            byte[] dllBytes = dll.bytes;
+            fixed (byte* ptr = dllBytes)
+            {
+                // 加载assembly对应的dll，会自动为它hook。一旦aot泛型函数的native函数不存在，用解释器版本代码
+                int err = Huatuo.HuatuoApi.LoadMetadataForAOTAssembly((IntPtr)ptr, dllBytes.Length);
+                Log.Info($"LoadMetadataForAOTAssembly:{assetName}. ret:{err}");
+            }
+            AOTLoadFlag++;
+            if (AOTLoadFlag == AOTFlag)
+            {
+                StartHotfix();
+            }
+        }
+
         private static void OnLoadAssetSuccess(string assetName, object asset, float duration, object userdata)
         {
-            GameObject game = Object.Instantiate((GameObject)asset);
+            GameObject game = UnityEngine.Object.Instantiate((GameObject)asset);
             game.name = "Game";
 
             HPBar = game.GetComponentInChildren<HPBarComponent>();
@@ -47,8 +106,7 @@ namespace Game.Hotfix
 
         private static void OnLoadAssetFail(string assetName, LoadResourceStatus status, string errormessage, object userdata)
         {
-            Log.Error("Load game failed. {0}", errormessage);
+            Log.Error("Load asset failed. {0}", errormessage);
         }
-
     }
 }
